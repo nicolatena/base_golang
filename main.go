956f6a0e74151816fe93c46981577595737074be
Nico P. Latena <nico@clappingape.com>
2019-04-08 17:07:43 +0700

    remove script static server frontend

--- main.go
+++ main.go
@@ -11,18 +11,9 @@ import (
 
 func main() {
 	route := gin.Default()
-
-	// untuk mendeteksi javascript, css, image di puclic
-	route.Use(static.Serve("/frontend", static.LocalFile("go_rest_api/frontend", true)))
+	
 	// inisialisasi folder upload
 	route.Use(static.Serve("/service", static.LocalFile("go_rest_api/service", true)))
-	// untuk render file template HTML
-	route.HTMLRender = gintemplate.New(gintemplate.TemplateConfig{
-		Root:         "go_rest_api/frontend/view/html",
-		Extension:    ".tmpl",
-		Master:       "support/master",
-		DisableCache: true,
-	})
 
 	routes.Routes(route)
 
